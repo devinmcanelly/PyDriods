b72ab7d5e06e49ba1700f5b55f6fed7d36d14f51
Devin McAnelly <devinmcanelly@outlook.com>
2024-09-03 21:44:43 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import constants
+# print here
+def main():
+	print(f"""Starting asteroids!
+Screen width: {constants.SCREEN_WIDTH}
+Screen height: {constants.SCREEN_HEIGHT}""")
+
+
+if __name__ == "__main__":
+	main()
+	
