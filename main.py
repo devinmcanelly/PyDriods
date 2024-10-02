fbfde6c5ab4f8b607ddd30613f487bc8bfeb0afe
Devin McAnelly <devinmcanelly@outlook.com>
2024-10-01 23:51:23 -0500

    sprite groups working

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from constants import *
 from player import Player
 
 def main():
-        print(f"""Starting asteroids!
-Screen width: {SCREEN_WIDTH}
-Screen height: {SCREEN_HEIGHT}""")
+        print(f"Starting asteroids!"
+              f"Screen width: {SCREEN_WIDTH} "
+              f"Screen height: {SCREEN_HEIGHT}"
+              )
         
         pygame.init()
         clock = pygame.time.Clock()
@@ -15,16 +16,44 @@ Screen height: {SCREEN_HEIGHT}""")
         updatables = pygame.sprite.Group()
         drawables = pygame.sprite.Group()
         player.containers = (updatables, drawables)
-        print(player.containers)
+        drawables.add(player)
+        updatables.add(player)
+        
         while True:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 return 
                 screen.fill((0,0,0,0))
-                player.draw(screen)
-                player.update(dt)
+
+                for member in drawables:
+                        member.draw(screen)
+                for member in updatables:
+                        member.update(dt)
+
                 pygame.display.flip()
                 dt = clock.tick(60) / 1000
+
+
+
 if __name__ == "__main__":
 	main()
 
+
+
+
+def test():
+        print(f"Starting asteroid test!"
+              f"Screen width: {SCREEN_WIDTH} "
+              f"Screen height: {SCREEN_HEIGHT}"
+              ) # This looks less bad I guess
+        
+        pygame.init()
+        clock = pygame.time.Clock()
+        dt = 0 # Delta-Time
+        # screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        updatables = pygame.sprite.Group()
+        drawables = pygame.sprite.Group()
+        player.containers = (updatables,drawables)
+        print(f'clock = {clock}, dt = {dt}, player containers {player.containers[0]}')
+        return player
